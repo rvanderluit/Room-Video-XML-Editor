90020e33e1a22ee41cae375c328ae524f25141f2
rvanderluit <rvanderluit@gmail.com>
2017-12-29 14:37:18 -0800

    from pycharm fixed function names to PEP8

--- AVeditor.py
+++ AVeditor.py
@@ -5,21 +5,23 @@ import os
 from lxml import etree
 
 room_numbers = []
-#index=0
-rooms = [] # room class objects
-##XMLParser = etree.XMLParser(remove_blank_text=True)
+# index=0
+rooms = []  # room class objects
+# XMLParser = etree.XMLParser(remove_blank_text=True)
+
+
 class Room:
         def __init__(self):
-                self.Name = ""
-                self.Number = None
-                self.VidSwitcherOutputNum = 0
-                self.HasLocalRec = 0
-                self.RecHasVolFB = 0
-                self.RecInputCommandDelay = 0
-                self.RecHasDistAudio = 0
-                self.AudioZoneNumber = ""
-                self.AudioHasVolFB = None
-                self.VidVolThroughDistAudio = None
+                self.name = ""
+                self.number = None
+                self.vid_switcher_output_num = 0
+                self.has_local_rec = 0
+                self.rec_has_vol_fb = 0
+                self.rec_input_command_delay = 0
+                self.rec_has_dist_audio = 0
+                self.audio_zone_number = ""
+                self.audio_has_vol_fb = None
+                self.vid_vol_through_dist_audio = None
                 self.LiftScenarioNum = None
                 self.OpenWithOnCmdNum = None
                 self.CloseWithOffCmdNum = None
@@ -28,7 +30,7 @@ class Room:
                
 
 ####FUNCTION DEFINITIONS#######
-def readXML():
+def read_xml():
         tree = etree.parse('roomAVDistribution.xml')
         root = tree.getroot()
         index = int(listbox.curselection()[0])
@@ -36,142 +38,157 @@ def readXML():
         room = roomz.find('room[@number="%s"]' % (index+1))
         print(room.get('name'))
         
-        #lists = filter(lambda x: 'Room' in x.get('name'), rooms.findall(".//room[@name]"))
-        #print(lists)
-        #for room in rooms:
-                #print(room.tag, room.attrib)#tag is just the element name
-def writeXML():
+        # lists = filter(lambda x: 'Room' in x.get('name'), rooms.findall(".//room[@name]"))
+        # print(lists)
+        # for room in rooms:
+                # print(room.tag, room.attrib)#tag is just the element name
+        
+        
+def write_xml():
         tree = etree.parse('roomAVDistribution.xml')
         root = tree.getroot()
         index = int(listbox.curselection()[0])
         roomz = root.find('rooms')
-        room = roomz.find('room[@number="%s"]' % (index+1)) #move to the right room
-        room.set('name', rooms[index].Name) #update the room name
-        config = room.find('configuration') #move to the configuration section
-        config.set('videoSwitcherOutputNum', rooms[index].VidSwitcherOutputNum)
-        ####LOCAL AVR SECTION####
-        rec = config.find('receiver') #move to the local AVR section
-        rec.set('hasReceiver', str(rooms[index].HasLocalRec))
-        rec.set('receiverHasVolFB', str(rooms[index].RecHasVolFB))
-        rec.set('receiverInputDelay', rooms[index].RecInputCommandDelay)
-        rec.set('musicThroughReceiver', str(rooms[index].RecHasDistAudio))
-        ####MUSIC#######
+        room = roomz.find('room[@number="%s"]' % (index+1))  # move to the right room
+        room.set('name', rooms[index].Name)  # update the room name
+        config = room.find('configuration')  # move to the configuration section
+        config.set('videoSwitcherOutputNum', rooms[index].vid_switcher_output_num)
+        # LOCAL AVR SECTION####
+        rec = config.find('receiver') # move to the local AVR section
+        rec.set('hasReceiver', str(rooms[index].has_local_rec))
+        rec.set('receiverHasVolFB', str(rooms[index].rec_has_vol_fb))
+        rec.set('receiverInputDelay', rooms[index].rec_input_command_delay)
+        rec.set('musicThroughReceiver', str(rooms[index].rec_has_dist_audio))
+        # MUSIC#######
         music = config.find('music')
-        music.set('musicZoneNum', rooms[index].AudioZoneNumber)
-        music.set('musicHasVolFB', str(rooms[index].AudioHasVolFB))
-        music.set('videoVolThroughDistAudio', str(rooms[index].VidVolThroughDistAudio))
-        ####WRITE TO THE FILE######
+        music.set('musicZoneNum', rooms[index].audio_zone_number)
+        music.set('musicHasVolFB', str(rooms[index].audio_has_vol_fb))
+        music.set('videoVolThroughDistAudio', str(rooms[index].vid_vol_through_dist_audio))
+        # WRITE TO THE FILE######
         tree.write('roomAVDistribution.xml')
         print('ok')
+         
                 
-def addRoomButton():
-        roomNumber = lowestOpenRoomNumber()
-        listbox.insert(roomNumber-1, roomNumber)
-        room_numbers.insert(roomNumber-1, roomNumber)
-        rooms.insert(roomNumber-1, Room())
-        rooms[roomNumber-1].Number = roomNumber
-        print('added room# %d' %(roomNumber))
+def add_room_button():
+        room_number = lowest_open_room_number()
+        listbox.insert(room_number-1, room_number)
+        room_numbers.insert(room_number-1, room_number)
+        rooms.insert(room_number-1, Room())
+        rooms[room_number-1].number = room_number
+        print('added room# %d' % room_number)
         print('room_numbers %s' % room_numbers)
 
-def roomButtonClick(evt):
+
+def room_button_click(evt):
         w = evt.widget
         index = int(w.curselection()[0])
         roomNameField.set(rooms[index].Name)
-        vidSwitchField.set(rooms[index].VidSwitcherOutputNum)
-        localRX.set(rooms[index].HasLocalRec)
-        localRXVisible()
+        vidSwitchField.set(rooms[index].vid_switcher_output_num)
+        localRX.set(rooms[index].has_local_rec)
+        local_rx_visible()
         value = w.get(index)
         
         print ('You selected item %d: "%s"' % (index, value))
+       
         
 def remove_button():
         index = int(listbox.curselection()[0])
         print('removed index %d' % (index))
         listbox.delete(index)
         del rooms[index]
-        del room_numbers[index] #removes by index
-        if(index > 0):
-                listbox.selection_set(index-1) #update the listbox selection
+        del room_numbers[index]  # removes by index
+        if index > 0:
+                listbox.selection_set(index-1)  # update the listbox selection
         else:
                 listbox.selection_set(0)
-        #room_numbers.remove(currentListSelection)#removes by value
+        # room_numbers.remove(currentListSelection)#removes by value
+        
         
 def move_up():
-        a = lowestOpenRoomNumber()
+        a = lowest_open_room_number()
         print(a)
+
 
 def move_down():
         print(index)
 
+
 def update_room(): 
         index = int(listbox.curselection()[0])
-        if (index >= 0):
+        if index >= 0:
                 listbox.delete(index)
-                rooms[index].Name = roomNameTextField.get()
-                rooms[index].VidSwitcherOutputNum = vidSwitchTextField.get()
-                rooms[index].HasLocalRec = localRX.get()
-                rooms[index].RecHasVolFB = localRXVolFb.get()
-                rooms[index].RecInputCommandDelay = recInputDelayTextField.get()
-                rooms[index].RecHasDistAudio = localRXDistAudio.get()
-                rooms[index].AudioZoneNumber = audioZoneField.get()
-                rooms[index].AudioHasVolFB = audioZoneVolFb.get()
-                rooms[index].VidVolThroughDistAudio = vidVolDist.get()
+                rooms[index].name = roomNameTextField.get()
+                rooms[index].vid_switcher_output_num = vidSwitchTextField.get()
+                rooms[index].has_local_rec = localRX.get()
+                rooms[index].rec_has_vol_fb = localRXVolFb.get()
+                rooms[index].rec_input_command_delay = recInputDelayTextField.get()
+                rooms[index].rec_has_dist_audio = localRXDistAudio.get()
+                rooms[index].audio_zone_number = audioZoneField.get()
+                rooms[index].audio_has_vol_fb = audioZoneVolFb.get()
+                rooms[index].vid_vol_through_dist_audio = vidVolDist.get()
                 listbox.insert(index, str(index+1) + " " + rooms[index].Name)
                 print(rooms[index].Name)
         listbox.selection_set(index)
 
 
-def printButtonList():
+def print_button_list():
         print(rooms)
 
-def lowestOpenRoomNumber():
+
+def lowest_open_room_number():
         if not room_numbers:
                 return 1
         else:
                 return next(i for i, e in enumerate(sorted(room_numbers) + [ None ], 1) if i != e)
 
-def printRoomList():
+
+def print_room_list():
         print(room_numbers)
-        
+
+
 def yview(self, *args):
         apply(listbox.yview, args)
         
-def localRXVisible():
-        RXvis = localRX.get()
-        if RXvis:
+        
+def local_rx_visible():
+        rx_is_visible = localRX.get()
+        if rx_is_visible:
                 recFrame.grid()
         else:
                 recFrame.grid_remove()
+             
                 
-def liftVisible():
-        liftIsVis = liftVis.get()
-        if liftIsVis:
+def lift_visible():
+        lift_is_visible = liftVis.get()
+        if lift_is_visible:
                 liftFrame.grid()
         else:
                 liftFrame.grid_remove()
+ 
                 
-def sleepVisible():
-        sleepIsVis = sleepVis.get()
-        if sleepIsVis:
+def sleep_visible():
+        sleep_is_visible = sleepVis.get()
+        if sleep_is_visible:
                 sleepFrame.grid()
         else:
                 sleepFrame.grid_remove()
 
-def surroundVisible():
-        surroundIsVis = surroundVis.get()
-        if surroundIsVis:
+
+def surround_visible():
+        surround_is_visible = surroundVis.get()
+        if surround_is_visible:
                 surroundFrame.grid()
         else:
                 surroundFrame.grid_remove()
-####END FUNCTION DEFINITIONS#######                
+# END FUNCTION DEFINITIONS #######                
 
 
-####START UI#######################
+# START UI #######################
 root = tk.Tk()
 root.title("ACS Room Video Editor v0.1")
 
-####FRAMES
-####frame1 top left
+# FRAMES
+# frame1 top left
 frame1 = tk.Frame(root, width=200, height=300, borderwidth=1, relief="groove")
 frame1.grid(row=0, column=0, sticky=tk.N)
 frameRight = tk.Frame(root, width=300, height=300, borderwidth=1, relief="groove")
@@ -192,12 +209,12 @@ surroundFrame.grid(row=16, column=0, columnspan=2)
 surroundFrame.grid_remove()
 
 ####FRAME1 WIDGETS
-add_room = tk.Button(frame1, text="add a room", width=15, command=addRoomButton)
+add_room = tk.Button(frame1, text="add a room", width=15, command=add_room_button)
 add_room.pack(pady =20, padx =20)
 
 
 listbox = tk.Listbox(frame1, exportselection=0)
-listbox.bind('<<ListboxSelect>>', roomButtonClick)
+listbox.bind('<<ListboxSelect>>', room_button_click)
 listbox.pack(side=tk.LEFT, fill=tk.Y)
 scrollbar = tk.Scrollbar(frame1, orient=tk.VERTICAL, command=listbox.yview)
 scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
@@ -206,8 +223,8 @@ listbox.config(yscrollcommand=scrollbar.set)
 
 ####FRAME2 WIDGETS LOWER LEFT
 delete_room = tk.Button(frame2, text="remove room", width=10, command=remove_button).grid(row=0, column=1)
-readXMLButton = tk.Button(frame2, text="read xml", width=7, command=readXML).grid(row=0, column=0)
-writeXMLButton = tk.Button(frame2, text="write xml", width=7, command=writeXML).grid(row=0, column=2)
+read_xmlButton = tk.Button(frame2, text="read xml", width=7, command=read_xml).grid(row=0, column=0)
+write_xmlButton = tk.Button(frame2, text="write xml", width=7, command=write_xml).grid(row=0, column=2)
 
 ####RIGHT FRAME WIDGETS
 roomNameLabel = tk.Label(frameRight, text="Room Name:").grid(row=0, column=0, sticky=tk.E)
@@ -224,8 +241,8 @@ vidSwitchTextField.grid(row=1, column=1, sticky=tk.W)
 localRecLabel = tk.Label(frameRight, text="Does this room have a local receiver?").grid(row=2, column=0, sticky=tk.E)
 localRX = tk.IntVar()
 localRX.set(0)
-tk.Radiobutton(frameRight, text="Yes", variable=localRX, command=localRXVisible, value=1).grid(row=2, column=1, sticky=tk.W)
-tk.Radiobutton(frameRight, text="No", variable=localRX, command=localRXVisible, value=0).grid(row=2, column=1)
+tk.Radiobutton(frameRight, text="Yes", variable=localRX, command=local_rx_visible, value=1).grid(row=2, column=1, sticky=tk.W)
+tk.Radiobutton(frameRight, text="No", variable=localRX, command=local_rx_visible, value=0).grid(row=2, column=1)
 
 localRecVolLabel = tk.Label(recFrame, text="Does this receiver have volume feedback?")
 localRecVolLabel.grid(row=0, column=0, sticky=tk.E)
@@ -280,8 +297,8 @@ tk.Radiobutton(frameRight, text="No", variable=displayVol, value=False).grid(row
 liftLabel = tk.Label(frameRight, text="Does the display in this room have a lift?")
 liftLabel.grid(row=9, column=0, sticky=tk.E)
 liftVis = tk.IntVar()
-tk.Radiobutton(frameRight, text="Yes", variable=liftVis, command=liftVisible, value=True).grid(row=9, column=1, sticky=tk.W)
-tk.Radiobutton(frameRight, text="No", variable=liftVis, command=liftVisible, value=False).grid(row=9, column=1)
+tk.Radiobutton(frameRight, text="Yes", variable=liftVis, command=lift_visible, value=True).grid(row=9, column=1, sticky=tk.W)
+tk.Radiobutton(frameRight, text="No", variable=liftVis, command=lift_visible, value=False).grid(row=9, column=1)
 
 liftScenario = tk.Label(liftFrame, text="Enter the lift page scenario number:")
 liftScenario.grid(row=1, column=0, sticky=tk.E)
@@ -336,8 +353,8 @@ pulseTimeTextField5.grid(row=8, column=3, sticky=tk.W)
 sleepLabel = tk.Label(frameRight, text="Will this room have a sleep timer?")
 sleepLabel.grid(row=12, column=0, sticky=tk.E)
 sleepVis = tk.IntVar()
-tk.Radiobutton(frameRight, text="Yes", variable=sleepVis, command=sleepVisible, value=True).grid(row=12, column=1, sticky=tk.W)
-tk.Radiobutton(frameRight, text="No", variable=sleepVis, command=sleepVisible, value=False).grid(row=12, column=1)
+tk.Radiobutton(frameRight, text="Yes", variable=sleepVis, command=sleep_visible, value=True).grid(row=12, column=1, sticky=tk.W)
+tk.Radiobutton(frameRight, text="No", variable=sleepVis, command=sleep_visible, value=False).grid(row=12, column=1)
 
 sleepScenario = tk.Label(sleepFrame, text="Enter the sleep page scenario number:")
 sleepScenario.grid(row=1, column=0, sticky=tk.E)
@@ -367,14 +384,14 @@ sleepTimeTextField3 = tk.Entry(sleepFrame, width=3)
 sleepTimeTextField3.grid(row=6, column=2, sticky=tk.W)
 sleepTimeTextField4 = tk.Entry(sleepFrame, width=3)
 sleepTimeTextField4.grid(row=7, column=2, sticky=tk.W)
-####End Sleep Timer#######
+# End Sleep Timer #######
 
-######Surround / Video Formats #######
+# Surround / Video Formats #######
 surroundLabel = tk.Label(frameRight, text="Will this room have surround or video format selections?")
 surroundLabel.grid(row=15, column=0, sticky=tk.E)
 surroundVis = tk.IntVar()
-tk.Radiobutton(frameRight, text="Yes", variable=surroundVis, command=surroundVisible, value=True).grid(row=15, column=1, sticky=tk.W)
-tk.Radiobutton(frameRight, text="No", variable=surroundVis, command=surroundVisible, value=False).grid(row=15, column=1)
+tk.Radiobutton(frameRight, text="Yes", variable=surroundVis, command=surround_visible, value=True).grid(row=15, column=1, sticky=tk.W)
+tk.Radiobutton(frameRight, text="No", variable=surroundVis, command=surround_visible, value=False).grid(row=15, column=1)
 
 surroundScenario = tk.Label(surroundFrame, text="Enter the surround/format page scenario number:")
 surroundScenario.grid(row=1, column=0, sticky=tk.E)
@@ -390,9 +407,9 @@ surroundText = tk.Label(surroundFrame, text="Enter the command number for each o
 surroundText.grid(row=4, column=0, sticky=tk.E)
 
 
-update = tk.Button(frameRight, text="update room", width = 15, command=update_room).grid(row=30, column=0)
-printBtns = tk.Button(frameRight, text="print buttons", width =15, command=printButtonList).grid(row=31, column=0)
-printRooms = tk.Button(frameRight, text="print Rooms", width = 15, command=printRoomList).grid(row=32, column=0)
+update = tk.Button(frameRight, text="update room", width=15, command=update_room).grid(row=30, column=0)
+printBtns = tk.Button(frameRight, text="print buttons", width=15, command=print_button_list).grid(row=31, column=0)
+printRooms = tk.Button(frameRight, text="print Rooms", width=15, command=print_room_list).grid(row=32, column=0)
 
 
 root.mainloop()
